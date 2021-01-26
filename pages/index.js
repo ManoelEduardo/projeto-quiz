73cf609475f3a40121868f4b84dd046fb3b17667
Manoel Eduardo <manoeleduardo.sistemas@gmail.com>
2021-01-26 19:57:51 -0300

    segunda video aula

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+
 import db from '../db.json';
 import Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo';
 import QuizBackground from '../src/components/QuizBackground';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
-
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -17,19 +21,37 @@ export const QuizContainer = styled.div`
   }
 `;
 
-
-
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>Imersão-React-Alura</title>
+      </Head>
       <QuizContainer>
-        
+        <QuizLogo />
         <Widget>
           <Widget.Header>
             <h1>Testando</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>dnklandasdbasdbkjasdkasdbjasbdkabsda</p>
+            <form onSubmit={function (e) {
+              e.preventDefault();
+              router.push(`/quiz?name=${name}`);
+            }}
+            >
+              <input
+                onChange={function (e) {
+                  setName(e.target.value);
+                }}
+                placeholder="Informe seu nome"
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar
+                {name}
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
 
@@ -38,12 +60,18 @@ export default function Home() {
             <h1>Testando</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>dnklandasdbasdbkjasdkasdbjasbdkabsda</p>
+            <p>
+              Lorem Ipsum é simplesmente uma simulação de texto da
+              indústria tipográfica e de impressos, e vem sendo utilizado
+              desde o século XVI, quando um impressor desconhecido pegou
+              uma bandeja de tipos e os embaralhou para fazer um livro de
+              modelos de tipos.
+            </p>
           </Widget.Content>
         </Widget>
-      <Footer />
+        <Footer />
       </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/ManoelEduardo"/>
+      <GitHubCorner projectUrl="https://github.com/ManoelEduardo" />
     </QuizBackground>
 
   );
