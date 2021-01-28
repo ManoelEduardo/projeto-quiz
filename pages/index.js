22a7800af2449b4dc520e806f618f856484c43ed
Manoel Eduardo <manoeleduardo.sistemas@gmail.com>
2021-01-28 00:06:00 -0300

    terceira video aula

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import styled from 'styled-components';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 
@@ -7,19 +6,11 @@ import db from '../db.json';
 import Widget from '../src/components/Widget';
 import QuizLogo from '../src/components/QuizLogo';
 import QuizBackground from '../src/components/QuizBackground';
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
-
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`;
+import QuizContainer from '../src/components/QuizContainer';
 
 export default function Home() {
   const router = useRouter();
@@ -36,21 +27,22 @@ export default function Home() {
             <h1>Testando</h1>
           </Widget.Header>
           <Widget.Content>
-            <form onSubmit={function (e) {
+            <form onSubmit={(e) => {
               e.preventDefault();
               router.push(`/quiz?name=${name}`);
             }}
             >
-              <input
-                onChange={function (e) {
+              <Input
+                name="nomeDoUsuario"
+                onChange={(e) => {
                   setName(e.target.value);
                 }}
                 placeholder="Informe seu nome"
+                value={name}
               />
-              <button type="submit" disabled={name.length === 0}>
-                Jogar
-                {name}
-              </button>
+              <Button type="submit" disabled={name.length === 0}>
+                {`Jogar ${name}`}
+              </Button>
             </form>
           </Widget.Content>
         </Widget>
---
+++ src/components/LoadingScreen/index.js
@@ -0,0 +1,9 @@
+/* eslint-disable linebreak-style */
+import React from 'react';
+import ReactLoading from 'react-loading';
+
+const LoadingScreen = () => (
+  <ReactLoading type="spin" color="#FFF" height="30%" width="30%" />
+);
+
+export default LoadingScreen;
